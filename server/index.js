61029a21dc7475f2895b68a572504a7999958ad7
batuhan <haymana.batuhan@gmail.com>
2021-04-16 23:46:46 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("db baglantisi basarili"))
+    .catch((err) => console.log(err));
+});
